00cbbabc66fa161663a665ed95b50252949925de
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2023-01-27 05:56:31 +0530

    Initial Commit

---
+++ ZingAssesment/ZingAssesmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZingAssesmentApp.swift
+//  ZingAssesment
+//
+//  Created by Adarsh Shukla on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ZingAssesmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
